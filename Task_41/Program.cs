024f9e41221886e3b3caa05c2630851e54c181cb
Arina <arinacypcova@gmail.com>
2023-07-22 18:09:57 +0700

    Added Task_41

---
+++ Task_41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int GetCountPositiveNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = GetInput($"Введите {i + 1} элемент массива: ");
+    }
+    return array;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int length = GetInput("Введите количество элемнтов: ");
+int[] array;
+array = InputArray(length);
+
+Console.WriteLine($"{String.Join(", ", (array))} -> {GetCountPositiveNum(array)}");
